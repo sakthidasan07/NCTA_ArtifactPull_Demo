9456ea5f8ad7f83561e22f46d508b7b46571ed60
sakthidasan07 <68719748+sakthidasan07@users.noreply.github.com>
2020-10-22 14:05:19 -0400

    Update script_sample_jenkinstest.sh

--- script_sample_jenkinstest.sh
+++ script_sample_jenkinstest.sh
@@ -5,7 +5,7 @@ echo "*************Writing parameter values*****************************"
 echo "Artifact parameter value passed is, $Artifact_ID"
 echo "Artifact type value passed is, $Artifact_Type"
 echo "*************Finished writing parameter values to log*************"
-if [ ! -n "$Artifact_ID" ]
+if [ ! -n "$ArtifactID" ]
 then
         echo "Error \$Artifact_ID not set or NULL"
 elif 
