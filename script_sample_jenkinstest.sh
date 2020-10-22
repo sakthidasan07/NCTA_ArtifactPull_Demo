50e54a2ca6dc7a21b1cf5b86b1e5c6419fd85d72
sakthidasan07 <68719748+sakthidasan07@users.noreply.github.com>
2020-10-22 13:23:46 -0400

    Update script_sample_jenkinstest.sh

--- script_sample_jenkinstest.sh
+++ script_sample_jenkinstest.sh
@@ -5,7 +5,8 @@ echo "Writing parameter values"
 echo "Artifact parameter value passed is, $Artifact_ID"
 echo "Artifact type value passed is, $Artifact_Type"
 echo "Finished writing parameter values to log"
-if [ -z "$Artifact_ID" ]; then
+if [ -z $Artifact_ID ]
+then
 echo "Empty Artifact ID. Please enter value and retry"
 exit 1
 else
