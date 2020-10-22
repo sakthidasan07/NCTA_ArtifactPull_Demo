7c032f7eab44d7890c6b0f481d3e31fbf9786384
sakthidasan07 <68719748+sakthidasan07@users.noreply.github.com>
2020-10-22 12:54:36 -0400

    Update script_sample_jenkinstest.sh
    User: Sakthidasan
    Date: 10/22/2020
    Note: Removed extra spaces in the "IF Condition"

--- script_sample_jenkinstest.sh
+++ script_sample_jenkinstest.sh
@@ -5,11 +5,10 @@ echo "Writing parameter values"
 echo "Artifact parameter value passed is, $Artifact_ID"
 echo "Artifact type value passed is, $Artifact_Type"
 echo "Finished writing parameter values to log"
-if [ -z "$Artifact_ID" ]
+if [-z "$Artifact_ID"]
 then
       echo "\$Artifact_ID is empty. Please enter value and retry"
       exit 1
-
 else
       echo "Connecting to repository"
       echo "Script execution completed"
