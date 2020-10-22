0cc116e3f37743bb83ccac01509e2f3e56c32b26
sakthidasan07 <68719748+sakthidasan07@users.noreply.github.com>
2020-10-22 17:10:46 -0400

    Update script_sample_jenkinstest.sh

--- script_sample_jenkinstest.sh
+++ script_sample_jenkinstest.sh
@@ -5,11 +5,10 @@ Artifact_Type="$Artifact_Type"
 echo "Artifact parameter value passed is, $Artifact_ID"
 echo "Artifact type value passed is, $Artifact_Type"
 echo "*************Finished writing parameter values to log*************"
-if [ -z "${ArtifactID:?}" ]
+if [[ -z "${ArtifactID:?}" ]]
 then
 echo "Sucessfull"
 else 
 echo "Unsucessfull" 
-exit 1
 fi
 echo "**************Script execution completed***************************"
