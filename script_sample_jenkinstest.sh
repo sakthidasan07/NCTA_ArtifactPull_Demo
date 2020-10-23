20e11c01ccd7f9a5a1280e02931df6962104e649
sakthidasan07 <68719748+sakthidasan07@users.noreply.github.com>
2020-10-23 11:56:27 -0400

    Add files via upload

---
+++ script_sample_jenkinstest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "*************Writing parameter values*****************************"
+Artifact_ID=$1
+Artifact_Type=$2
+echo "Artifact parameter value passed is, $Artifact_ID"
+echo "Artifact type value passed is, $Artifact_Type"
+echo "*************Finished writing parameter values to log*************"
+if [ $1 == $2 ]
+then
+    echo "YES"
+else
+    echo "NO"
+fi
