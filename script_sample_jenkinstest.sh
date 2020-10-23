294c20b52af9ec4839ea4e5a1849023c340f19dc
sakthidasan07 <68719748+sakthidasan07@users.noreply.github.com>
2020-10-23 11:26:55 -0400

    Update script_sample_jenkinstest.sh

--- script_sample_jenkinstest.sh
+++ script_sample_jenkinstest.sh
@@ -5,9 +5,9 @@ Artifact_Type=$2
 echo "Artifact parameter value passed is, $Artifact_ID"
 echo "Artifact type value passed is, $Artifact_Type"
 echo "*************Finished writing parameter values to log*************"
-if [ $1 == $2 ] 
-then 
+if [ $1 == $2 ]
+then
     echo "YES"
-else 
+else
     echo "NO"
 fi
